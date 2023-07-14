0b86b34d2b689a0cfd6f7e8816d2de6150c72ba7
kaushalbharade <130897832+kaushalbharade@users.noreply.github.com>
2023-07-14 18:06:56 +0530

    Add files via upload

---
+++ DAY 1.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[43]:
+
+
+a=20
+#a=variable
+#==assignment operator
+#20=data
+
+
+# In[44]:
+
+
+a
+
+
+# In[45]:
+
+
+print(a)
+
+
+# In[54]:
+
+
+a="Kaushal"
+a="""Kaushal"""
+a='''Kaushal'''
+
+
+# In[55]:
+
+
+a
+
+
+# In[59]:
+
+
+a='Kaushal'
+b='Bharade'
+
+
+# In[63]:
+
+
+c=a+b
+
+
+# In[64]:
+
+
+c
+
+
+# In[65]:
+
+
+print("Kaushal\nBharade")
+
+
+# In[66]:
+
+
+print("Kaushal\tBharade")
+
+
+# In[70]:
+
+
+a=20
+b="Kaushal"
+
+
+# In[71]:
+
+
+type(a)
+
+
+# In[72]:
+
+
+type(b)
+
+
+# In[ ]:
+
+
+
+
